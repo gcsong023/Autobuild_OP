e824276695ff9f1a85e3be02082a8c4e6757bb96
巴山夜语 <66949363+gcsong023@users.noreply.github.com>
2025-03-17 01:14:18 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -8,7 +8,7 @@ uci commit luci
 # sed -i 's/^[^#].*option ula/#&/' /etc/config/network
 
 # Check file system during boot
-# uci set fstab.@global[0].check_fs=1
-# uci commit fstab
+uci set fstab.@global[0].check_fs=1
+uci commit fstab
 
 exit 0
